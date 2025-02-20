b174bea7946383c12ad3ce6d28e2d17e68abd168
y-ren16 <2367050142@qq.com>
2025-02-21 00:18:54 +0800

    train shell

--- scripts/ade/ft_swinb_100-50-FixB.sh
+++ scripts/ade/ft_swinb_100-50-FixB.sh
@@ -7,17 +7,23 @@ start=`date +%s`
 START_DATE=$(date '+%Y-%m-%d')
 
 PORT=$((9000 + RANDOM % 1000))
-GPU=0,1,2,3
-NB_GPU=4
 
-DATA_ROOT=path/to/your/dataset/
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <GPU> <NB_GPU>"
+    exit 1
+fi
 
-DATASET=voc
-TASK=15-5s
-NAME=ours
+GPU=$1
+NB_GPU=$2
+
+DATA_ROOT=/home/renyong/ECCV24_NeST/data/PascalVOC12
+
+DATASET=ade
+TASK=100-50
+NAME=FT-SwinB-FixB
 METHOD=FT 
 # relevant parameters are set here
-OPTIONS="--backbone resnet101 --warm_up --warm_epochs 5 --warm_lr_scale 1 --unce_in_warm --checkpoint checkpoints/step/ --loss_kd 10 --unce --unkd --init_balanced"
+OPTIONS="--backbone swin_b --checkpoint checkpoints/step/ --fix_bachbone"
 
 
 SCREENNAME="${DATASET}_${TASK}_${NAME} On GPUs ${GPU}"
@@ -32,18 +38,21 @@ echo "Writing in ${RESULTSFILE}"
 # If you already trained the model for the first step, you can re-use those weights
 # in order to skip this initial step --> faster iteration on your model
 # Set this variable with the weights path
-# FIRSTMODEL=/path/to/my/first/weights
+FIRSTMODEL=/home/renyong/ECCV24_NeST/checkpoints/step/${TASK}-${DATASET}_FT_0.pth
 # Then, for the first step, append those options:
 # --ckpt ${FIRSTMODEL} --test
 # And for the second step, this option:
 # --step_ckpt ${FIRSTMODEL}
 
-BATCH_SIZE=6
+NEW_FIRSTMODEL=/home/renyong/ECCV24_NeST/checkpoints/step/${TASK}-${DATASET}_${NAME}_0.pth
+cp ${FIRSTMODEL} ${NEW_FIRSTMODEL}
+
+BATCH_SIZE=24
 INITIAL_EPOCHS=30
 EPOCHS=30
 
-CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 0 --lr 0.02 --epochs ${INITIAL_EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
-CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 1 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
+CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 0 --lr 0.02 --epochs ${INITIAL_EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS} --ckpt ${NEW_FIRSTMODEL} --test
+CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 1 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS} --step_ckpt ${NEW_FIRSTMODEL}
 CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 2 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
 CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 3 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
 CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 4 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
--- scripts/voc/ft_resnet101_15-1.sh
+++ scripts/voc/ft_resnet101_15-1.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 set -e
@@ -8,18 +7,23 @@ start=`date +%s`
 START_DATE=$(date '+%Y-%m-%d')
 
 PORT=$((9000 + RANDOM % 1000))
-GPU=0,1,2,3
-NB_GPU=4
 
-DATA_ROOT=path/to/your/dataset/
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <GPU> <NB_GPU>"
+    exit 1
+fi
+
+GPU=$1
+NB_GPU=$2
+
+DATA_ROOT=/home/renyong/ECCV24_NeST/data/PascalVOC12
 
 DATASET=voc
-TASK=10-1
-NAME=ours
+TASK=15-5s
+NAME=FT
 METHOD=FT 
 # relevant parameters are set here
-OPTIONS="--backbone resnet101 --warm_up --warm_epochs 5 --warm_lr_scale 1 --unce_in_warm --checkpoint checkpoints/step/ --loss_kd 10 --unce --unkd --init_balanced"
-
+OPTIONS="--backbone resnet101 --checkpoint checkpoints/step/"
 
 
 SCREENNAME="${DATASET}_${TASK}_${NAME} On GPUs ${GPU}"
@@ -40,9 +44,9 @@ echo "Writing in ${RESULTSFILE}"
 # And for the second step, this option:
 # --step_ckpt ${FIRSTMODEL}
 
-BATCH_SIZE=6
+BATCH_SIZE=24
 INITIAL_EPOCHS=30
-EPOCHS=10
+EPOCHS=30
 
 CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 0 --lr 0.02 --epochs ${INITIAL_EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
 CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 1 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
@@ -50,11 +54,6 @@ CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${
 CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 3 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
 CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 4 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
 CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 5 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
-CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 6 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
-CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 7 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
-CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 8 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
-CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 9 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
-CUDA_VISIBLE_DEVICES=${GPU} python3 -m torch.distributed.launch --master_port ${PORT} --nproc_per_node=${NB_GPU} run.py --date ${START_DATE} --data_root ${DATA_ROOT} --overlap --batch_size ${BATCH_SIZE} --dataset ${DATASET} --name ${NAME} --task ${TASK} --step 10 --lr 0.001 --epochs ${EPOCHS} --method ${METHOD} --opt_level O1 ${OPTIONS}
 python3 average_csv.py ${RESULTSFILE}
 
 echo ${SCREENNAME}
